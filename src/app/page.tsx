6fa4c0904e674d0bc1760f33ea347456a513636c
JuanMontealegre-Inlaze <j.montealegre@inlaze.com>
2024-08-15 15:45:22 -0500

    chore: Solve conflicts and merge

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "./layout";
+
+export default function Home(): JSX.Element {
+  return (
+    <RootLayout>
+      <h1>Welcome to the Inlaze Next.js Starter!</h1>
+    </RootLayout>
+  );
+}
