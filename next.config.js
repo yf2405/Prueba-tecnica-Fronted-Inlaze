b0922161435201f8ba2ebbbf08900b9440263117
Yerson <yersoncorrea278@gmail.com>
2024-09-08 23:51:42 -0500

    ultimoos cambios y funcionalidades

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Ignora ESLint durante la construcción
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
+    
   },
 };
 
